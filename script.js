a88a0a5a5bd2b5365d2a12161e3de2c5d9977bf5
Valerii <valeriy.stronskiy@gmail.com>
2023-06-14 14:23:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
+//Перша частина
 //Завдання №1
 /*let userName = prompt("Як Вас звати?","Наприклад: Василь");
-
 alert(`Привіт, ${userName}!`);*/
 
 //Завдання №2
 /*const currentYear = 2023;
 const usersBirthYear = prompt("Введіть рік Вашого народження","Наприклад: 2001");
-
 currentYear > usersBirthYear ? alert(`Ваш вік: ${currentYear - usersBirthYear} років`) : alert("Рік вказано некоректно!");*/
 
 //Завдання №3
 /*const userNumber = prompt("Введіть тризначне число", "Наприклад: 341");
-
 userNumber > 99 && userNumber < 1000 ? alert(`${userNumber%10}${Math.floor(userNumber/10%10)}${Math.floor(userNumber/100)}`) : alert("Значення введено некоректно!");*/
 
 //Завдання №4
 /*const userNumber = prompt("Введіть ціле чило","Наприклад: 12");
-
 userNumber % 2 ? alert("Число непарне!") : alert("Число парне!");*/
+//--------------------------------------------------------------------
+
+//Друга частина
+//Завдання №2
